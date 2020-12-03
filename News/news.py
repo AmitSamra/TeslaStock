c5bd2570ce5b402663b8e9c0d53feb58baf763bf
Amit Samra <Amit.Samra@outlook.com>
2020-12-03 01:12:33 -0500

    Change code to access articles daily

--- News/news.py
+++ News/news.py
@@ -4,6 +4,8 @@ import pandas as pd
 import yfinance as yf
 from newsapi import NewsApiClient
 import json
+import math
+from datetime import timedelta, datetime
 from dotenv import load_dotenv
 dotenv_local_path = '../.env'
 load_dotenv(dotenv_path=dotenv_local_path, verbose=True)
@@ -23,7 +25,7 @@ newsapi = NewsApiClient(api_key=os.environ.get("NEWS_API_KEY"))
 # Get total results needed for pagination
 news = newsapi.get_everything(
 q='Tesla',
-sources='bloomberg, reuters',
+sources='bloomberg',
 from_param='2020-11-03',
 to='2020-12-03',
 language='en',
@@ -31,18 +33,39 @@ sort_by='publishedAt',
 page_size=100
 )
 total_results=news["totalResults"]
+max_page = math.ceil(total_results/100)
 
 
-news = newsapi.get_everything(
-q='Tesla',
-sources='bloomberg, reuters',
-from_param='2020-11-03',
-to='2020-12-03',
-language='en',
-sort_by='publishedAt',
-page_size=100
-)
+"""
+NewAPI does not allow more than 100 results per request so we cannot use the code below.
+Instead, we must create a request for each day, assuming there are less than 100 articles per day.
 
-file_path = './articles.json'
-with open(file_path, 'w') as f:
-    json.dump(news, f, indent=4, sort_keys=True)
+# Get all articles and store in JSON file
+year = datetime.now().year
+month = datetime.now().month
+day = datetime.now().day
+
+start_date = '2020-11-03'
+end_date = '2020-12-03'
+
+page_count = 1
+
+while page_count <= 1:
+    news = newsapi.get_everything(
+    q='Tesla',
+    sources='bloomberg',
+    from_param=start_date,
+    to=end_date,
+    language='en',
+    sort_by='publishedAt',
+    page_size=100,
+    page=page_count
+    )
+    
+    file_path = f"./articles_{year}-{month}-{day}_{page_count}.json"
+    with open(file_path, 'w') as f:
+        json.dump(news, f, indent=4, sort_keys=True)
+    
+    page_count += 1
+"""
+
