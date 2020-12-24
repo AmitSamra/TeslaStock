95ffcbcaf49b2bad81eec389c6dc9fea094f169b
Amit Samra <Amit.Samra@outlook.com>
2020-12-24 01:39:12 -0500

    Added NLP task

--- Airflow/tsla.py
+++ Airflow/tsla.py
@@ -281,7 +281,79 @@ t4 = PythonOperator(
 )
 
 # ----------------------------------------------------------------------------------------------------
-# 
+# Perform NLP on headlines
+
+def nlp():
+	"""
+	Perofrms NLP on headlines obtained from NewsAPI.
+	Calculates a sentiment/polarity score for each headline.
+	Outputs a CSV with a sentiment/polarity score for each day.
+	"""
+
+	# Load headlines2 CSV
+	df = pd.read_csv('./headlines2.csv')
+
+	# Perform NLP
+	sid = SentimentIntensityAnalyzer()
+
+	for index, row in df.iterrows():
+	    title = row['title']
+	    token = regexp_tokenize(title, pattern=r"\s|[\.,;']", gaps=True)
+	    
+	    stop_words = stopwords.words('english')
+	    headline_filtered = []
+	    for i in token:
+	        if i not in stop_words:
+	            headline_filtered.append(i) 
+	    
+	    headline_pos = []
+	    headline_neu = []
+	    headline_neg = []
+	    
+	    for word in headline_filtered:
+	        if (sid.polarity_scores(word)['compound']) > 0:
+	            headline_pos.append(word)
+	        elif (sid.polarity_scores(word)['compound']) < 0:
+	            headline_neg.append(word)
+	        else:
+	            headline_neu.append(word)
+
+	    score = round((1*len(headline_pos) - 1*len(headline_neg) + 0*len(headline_neu))/len(headline_filtered),2)
+	    df.at[index, 'sentiment_score'] = score
+
+
+	# Create a copy of df_headlines to perform grouping operations
+	df2 = df.copy()
+	df2.drop(['source', 'title'], axis=1, inplace=True)
+
+	# Group headlines by date and calculate average score
+	df2_group = df2.groupby('date', as_index=False).mean()
+	df2_group = df2_group.rename(columns = {'sentiment_score': 'daily_sentiment_score'})
+
+	# Save df2_group to csv
+	df2_group.to_csv('sentiment_score.csv', index=False)
+
+
+t5 = PythonOperator(
+	task_id = 'nlp',
+	python_callable = nlp,
+	provide_context = False,
+	dag = dag
+)
+
+# ----------------------------------------------------------------------------------------------------
+# Perform NLP on headlines
+
+
+
+
+
+
+
+
+
+
+
 
 
 
